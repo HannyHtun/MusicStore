ad8e6a01dee2f3dbc90998c9863432f07ebf8a0b
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2016-11-04 01:57:39 +0800

    Firebase integrated

--- app/src/main/java/com/devfest/musicstore/MainActivity.java
+++ app/src/main/java/com/devfest/musicstore/MainActivity.java
@@ -2,12 +2,72 @@ package com.devfest.musicstore;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.devfest.musicstore.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mFireBaseAuth;
+    DatabaseReference mDatabase;
+    FirebaseDatabase mFireBaseDb;
+    boolean initialLoad = true;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        mFireBaseAuth.signInAnonymously();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("0").child("sections").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if(initialLoad){
+
+                    // TODO initial Fetch
+
+                    initialLoad = false;
+                }else{
+                    // TODO onChildAdded
+
+
+                }
+
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                Toast.makeText(MainActivity.this,"On Child AddedonChildChanged",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                Toast.makeText(MainActivity.this,"On Child Removed",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                Toast.makeText(MainActivity.this,"OnChild Moved",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
 }
