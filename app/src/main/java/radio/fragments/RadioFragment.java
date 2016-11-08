fb5df0aa21620a5a58871bb6bb1fbbdf4a881a73
တင်းထိုက်အောင် <winhtaikaung28@hotmail.com>
2016-11-09 02:17:02 +0800

    Radio Fragment Implementation on Progress

--- app/src/main/java/radio/fragments/RadioFragment.java
+++ app/src/main/java/radio/fragments/RadioFragment.java
@@ -3,14 +3,33 @@ package radio.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.devfest.musicstore.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import home.viewadapters.SectionViewAdapter;
+import model.Screen;
 
 public class RadioFragment extends Fragment {
 
+    RecyclerView mSectionRecyclerView;
+    LinearLayoutManager mLinearLayoutManager;
+    SectionViewAdapter mSectionViewAdapter;
+    Screen mScreen;
+
+    FirebaseAuth mFireBaseAuth;
+    DatabaseReference mDatabase;
+    boolean initialLoad = true;
     public RadioFragment() {
         // Required empty public constructor
     }
@@ -22,6 +41,83 @@ public class RadioFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_radio, container, false);
         return v;
 
+    }
+
+    void renderView(View v) {
+        mSectionRecyclerView = (RecyclerView) v.findViewById(R.id.sectionRecyclerView);
+
+        mSectionViewAdapter = new SectionViewAdapter(this.getActivity(), mScreen.getSections(), new SectionViewAdapter.SectionViewListener() {
+            @Override
+            public void onItemClickListener(String FilePath, int position, SectionViewAdapter.SectionViewHolder vh) {
+
+            }
+
+            @Override
+            public void onMenuClickListener(String FilePath, int position, SectionViewAdapter.SectionViewHolder vh) {
+
+            }
+        });
+        mLinearLayoutManager = new LinearLayoutManager(this.getActivity());
+        mLinearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        mSectionRecyclerView.setLayoutManager(mLinearLayoutManager);
+        mSectionRecyclerView.setAdapter(mSectionViewAdapter);
+    }
+
+    void firebase(final View v) {
+        mFireBaseAuth = FirebaseAuth.getInstance();
+        mFireBaseAuth.signInAnonymously();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("data").getParent().addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (initialLoad) {
+
+                    // TODO initial Fetch
+
+                    // now we get the Whole Screen and will pass the screen to Recycler Views
+
+                    for(DataSnapshot ds : dataSnapshot.getChildren()){
+
+
+                        if(ds.hasChild("title")){
+                            if(ds.child("title").getValue().toString().equals("Radio")){
+                                mScreen = new Screen(ds);
+
+                            }
+
+                        }
+
+
+                    }
+
+                    initialLoad = false;
+                } else {
+                    // TODO onChildAdded
+
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                // TODO play with data when onChildChanged
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                // TODO play with data when childElements were removed
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                // TODO play with data when child element is moved
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 }
